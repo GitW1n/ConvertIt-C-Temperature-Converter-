23023807e531803b56a6e38aa7a7fd8e1a1cb6e4
Yakov <167911620+GitW1n@users.noreply.github.com>
2024-12-03 22:48:07 +0300

    Update main.c

--- ConvTemps/main.c
+++ ConvTemps/main.c
@@ -23,7 +23,11 @@ int main() {
         printf("Temperature in Fahrenheit: %.2f\n", (temperature * 9.0 / 5.0) + 32);
     } else if (type == 'K' || type == 'k') {
         printf("Temperature in Kelvin: %.2f\n", temperature + 273.15);
-   
+    } else if (type == 'R' || type == 'r') {
+        printf("Temperature in Rankine: %.2f\n", (temperature + 273.15) * 9.0 / 5.0);
+    } else {
+        printf("Invalid type!!!\n");
+    }
 
     return 0;
 }
