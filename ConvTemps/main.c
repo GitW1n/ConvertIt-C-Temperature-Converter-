21bef65fdb30bf502d342c63ddf64826e7b2bc07
Yakov <167911620+GitW1n@users.noreply.github.com>
2024-12-03 22:49:25 +0300

    Update main.c

--- ConvTemps/main.c
+++ ConvTemps/main.c
@@ -8,7 +8,7 @@ int main() {
     printf("Enter temperature value: ");
     if (scanf("%f", &temperature) != 1) {
         printf("Invalid temperature input!\n");
-        return 1;// Return with error
+        return 1;
     }
 
     printf("Enter type (F for Fahrenheit, K for Kelvin, R for Rankine): ");
